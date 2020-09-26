e4a70648de9683ca99aeefb5006c9ef4dc5edd96
ophelia swen <ophelia.swen@gmail.com>
2020-09-26 14:49:16 -0700

    re-write logic since it wasn't reading the split function

--- lib/anagram_logic.rb
+++ lib/anagram_logic.rb
@@ -1,62 +1,34 @@
 require('pry')
 
 class Anagram
-  attr_accessor(:first_word, :second_word)
 
   def initialize (first_word, second_word)
-    @first_word = remove_s_p(first_word)
-    @second_word = remove_s_p(second_word)
+    @first_word = first_word
+    @second_word = second_word
+  end
+
+  def anagram_check()
+    first_word_clear = @first_word.downcase.gsub(/[^a-z]/i, '').split("")
+    second_word_clear = @second_word.downcase.gsub(/[^a-z]/i, '').split("")
+
+    if first_word_clear.any? {|letter| second_word_clear.include?(letter)}
+      if first_word_clear.sort == second_word_clear.sort
+      "Anagram detected!"
+      else
+      word_check()
+      end
+    else
+      "Antigram detected!"
+    end
   end
 
   def word_check()
     if (@first_word.match(/[aeiouy]/i) || @second_word.match(/[aeiouy]/i))
-       return "yes, it is a word"
+      "Your words contain vowels, yes, it is a word"
+    else
+      "'#{@first_word}' and '#{@second_word}' are not words."
     end
   end
 
-  def remove_s_p(word_combo)
-    return word_combo.downcase.gsub(/[^a-z\s]/i, '') 
-  end
-
-  def split_letters(word_choice)
-   word_choice.split(" ")
-  end
-
-  def antigram_check()
-    letter_match = []
-
-     first_word_letters = split_letters(@first_word)
-     second_word_letters = split_letters(@second_word)
-
-     if (letter_match.include?(true))
-      "false"
-     else
-      "Antigram detected!"
-    end
-  end
-  
-  def anagram_check()
-    second_word = @second_word.chars
-    first_word.chars.permutation.to_a.include?(second_word)
-      return "Anagram detected!"   
-    end
-  end
-
-
-#   def word_check()
-
-#       if (@first_word.include?(/[aeiou]/)) && (@second_word.include?(/[aeiou]/))
-#         return "yes, it is a word"
-#       else
-#         return "no, no word detected"
-#       end
-#     end
-
-  # def case_check()
-  #   if @first_word.downcase.split("").match == @second_word.downcase.split("").match
-  #     return "anagram detected"
-  #   else 
-  #     return "antigram detected"
-  #   end
-  # end
-
+  def
+end
--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -3,41 +3,42 @@ require('rspec')
 
 describe('Anagram#anagram_logic') do
 
-  it('will inform user that the selected two words is an antigram') do
-      word_select = Anagram.new('front','back')
-      expect(word_select.antigram_check).to(eq("Antigram detected!"))
-    end
-
   it('will inform user that the selected two words are anagrams') do
     word_select = Anagram.new('tar', 'rat')
     expect(word_select.anagram_check()).to(eq("Anagram detected!"))
   end
 
+  it('will inform user that the selected two words is an antigram') do
+    word_select = Anagram.new('front','back')
+    expect(word_select.anagram_check).to(eq("Antigram detected!"))
+  end
   it('will check for vowels to determine whether selected word is actually a word') do
     word_select = Anagram.new('state', 'taste')
-    expect(word_select.word_check()).to(eq("yes, it is a word"))
+    expect(word_select.word_check()).to(eq("Your words contain vowels, yes, it is a word"))
   end
+  it('will not account for case differences in selected word') do
+    word_select = Anagram.new('elbow','Below')
+    expect(word_select.Anagram).to(eq("Anagram check: True"))
+  end
+
 end
+
+
+ 
+# end
 # # end
 #   it('will inform user that the selected two words are not anagrams') do
 #     word_select = Anagram.new('dinosaur','rat')
 #     expect(word_select.anagram_check()).to(eq("Anagram check: False"))
 #   end
 
-#   it('will not account for case differences in selected word') do
-#     word_select = Anagram.new('elbow','Below')
-#     expect(word_select.Anagram).to(eq("Anagram check: True"))
-#   end
+  it('will not account for case differences in selected word') do
+    word_select = Anagram.new('elbow','Below')
+    expect(word_select.Anagram).to(eq("Anagram check: True"))
+  end
 
-#   it('will check for vowels to determine whether selected word is actually a word') do
-#     word_check = Anagram.new('state')
-#     expect(word_check.Anagram).to(eq("yes, it is a word"))
-#   end
 
-#   it('will inform user that the selected two words is an antigram') do
-#     word_select = Anagram.new('front','back')
-#     expect(word_select.Anagram).to(eq("Antigram detected!"))
-#   end
+
 
 #   it('will inform user that the selected words are anagrams') do
 #     word_select = Anagram.new('George Bush','He bugs Gore')
